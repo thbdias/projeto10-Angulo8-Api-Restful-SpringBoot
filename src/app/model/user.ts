ccc0a59d9ace77c2be2a8cf1fc4e345b270a18c3
thbd <thiagobalbino91@gmail.com>
2020-01-19 17:45:51 -0300

    class model e service do usuário

---
+++ src/app/model/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    
+    id: Number; //no back ende esse campo está como Long
+    login: String;
+    nome: String;
+    // cpf: String; //aula usa, porem back end nao possui
+}
